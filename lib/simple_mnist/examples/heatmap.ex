0c0c63dca5ad8b6cf27421a142b0f44b5421778f
Jia Wang <wj2016@gmail.com>
2024-01-26 18:08:28 +0100

    用训练出来的model运行测试数据

--- lib/simple_mnist/examples/heatmap.ex
+++ lib/simple_mnist/examples/heatmap.ex
@@ -19,6 +19,26 @@ defmodule SimpleMnist.Examples.Heatmap do
     end)
   end
 
+  # 72104
+  def demo2() do
+    <<_::32, n_images::32, n_rows::32, n_cols::32, images::binary>> =
+      read_and_unzip!("t10k-images-idx3-ubyte.gz")
+
+    all =
+      images
+      |> Nx.from_binary({:u, 8})
+      |> Nx.reshape({n_images, 1, n_rows, n_cols})
+      |> Nx.divide(255)
+
+    Enum.each(0..4, fn i ->
+      all
+      |> Nx.slice_along_axis(i, 1)
+      |> Nx.reshape({1, 1, 28, 28})
+      |> Nx.to_heatmap()
+      |> IO.inspect(label: "heatmap")
+    end)
+  end
+
   def demo_binary_to_heatmap() do
     <<_::32, _n::32, _r::32, _c::32, first_digit::binary-size(28 * 28), _::binary>> =
       read_and_unzip!("train-images-idx3-ubyte.gz")
--- lib/simple_mnist/examples/mnist_train.ex
+++ lib/simple_mnist/examples/mnist_train.ex
@@ -22,5 +22,70 @@ defmodule SimpleMnist.Examples.MnistTrain do
 
     IO.puts("The result of the first batch against the trained network")
     IO.inspect(EXLA.jit(&MNIST.predict/2).(final_params, hd(Enum.to_list(train_images))))
+    final_params
+  end
+
+  def run_test(params) do
+    {images, labels} = load_test()
+
+    Enum.each(0..10, fn i ->
+      image = Nx.slice_along_axis(images, i, 1)
+      label = Nx.slice_along_axis(labels, i, 1)
+      {image, label}
+
+      result =
+        MNIST.predict(params, image)
+        |> Nx.to_flat_list()
+        |> output_to_number()
+
+      expected =
+        label
+        |> Nx.to_flat_list()
+        |> output_to_number()
+
+      IO.puts("第#{i}个: Predict结果:#{result} 标准答案:#{expected}")
+    end)
+  end
+
+  def output_to_number(ten_nums) do
+    ten_nums
+    |> Enum.zip(0..9)
+    |> Enum.sort(:desc)
+    |> hd()
+    |> elem(1)
+  end
+
+  def load_test(
+        images_file \\ "t10k-images-idx3-ubyte.gz",
+        labels_file \\ "t10k-labels-idx1-ubyte.gz"
+      ) do
+    <<_::32, n_images::32, n_rows::32, n_cols::32, images::binary>> =
+      read_and_unzip!(images_file)
+
+    train_images =
+      images
+      |> Nx.from_binary({:u, 8})
+      |> Nx.reshape({n_images, n_rows * n_cols}, names: [:batch, :input])
+      |> Nx.divide(255)
+
+    IO.puts("#{n_images} #{n_rows}x#{n_cols} images\n")
+
+    <<_::32, n_labels::32, labels::binary>> = read_and_unzip!(labels_file)
+
+    train_labels =
+      labels
+      |> Nx.from_binary({:u, 8})
+      |> Nx.reshape({n_labels, 1}, names: [:batch, :output])
+      |> Nx.equal(Nx.tensor(Enum.to_list(0..9)))
+
+    IO.puts("#{n_labels} labels\n")
+
+    {train_images, train_labels}
+  end
+
+  defp read_and_unzip!(filename) do
+    filename
+    |> File.read!()
+    |> :zlib.gunzip()
   end
 end
